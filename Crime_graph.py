634e739aac6123c4ef04475d0fcfff37aa35a3cf
minimin227 <minimin227@gmail.com>
2025-04-28 20:05:37 +0900

    preprocess revision for SQL query

--- Crime_graph.py
+++ Crime_graph.py
@@ -350,8 +350,9 @@ try:
                 'severity_per_resolution_sum': 'Severity_per_resolution_sum',
                 'severity_per_resolution_mean': 'Severity_per_resolution_mean'
             }
-            st.write("df_group columns:", df_group.columns.tolist())
+            
             df_group = df_group.rename(columns=rename_mapping)
+            st.write("df_group columns:", df_group.columns.tolist())
         else:
             st.warning("선택된 컬럼이 없어 그룹화할 수 없습니다.")
             df_group = pd.DataFrame()
